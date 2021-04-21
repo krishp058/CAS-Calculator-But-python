80229eed3a238ba2800e773f6a4f0dcac8c60cd7
AVI0005 <>
2021-04-21 04:25:55 +0000

    changes

--- main.py
+++ main.py
@@ -1,30 +1,45 @@
 
 
 def start():
-  numbers = input("what are the  numbers you want to calculate seperated by a space: ")
-  return numbers
+    numbers = input("what are the  numbers you want to calculate seperated by a space: ")
+    return numbers
 
 
 def List(rawNumbers):
-  formatedNumbers = rawNumbers.split()
-  try:
-    formatedResult = [int(formatedNumbers) for formatedNumbers in formatedNumbers]
-  except:
-    print("error: Please input a number")
-  return (formatedResult)
+    formatedNumbers = rawNumbers.split()
+    try:
+        formatedResult = [int(formatedNumbers) for formatedNumbers in formatedNumbers]   
+    except:
+        print("error: Please input a number")
+    return (formatedResult)
   
 
 def operation(inputNumbers):
     operatation = input("What operation would you like?: ")
-
     result = 0
-  #operations
+    result_minus = 0
+    result_times = 1
+    
+    result_minus = inputNumbers[0]
+    #operations
     if operatation == "+":
         for number in inputNumbers:
             result += number
         print(result)
+
     elif operatation == "-":
+        for number in inputNumbers[1:]:
+            result_minus -= number
+        print(result_minus)
+
+    elif operatation == "*":
         for number in inputNumbers:
-            result_minus = number[0]
-            result_minus -= number[1:] 
+            result_times *= number 
+        print(result_times)
+
+    elif operatation == "/":
+        for number in inputNumbers:
+            result_times = inputNumbers[0] / inputNumbers[1] 
+        print(result_times)
+
 operation(List(start()))
