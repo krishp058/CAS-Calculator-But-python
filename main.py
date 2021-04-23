1bf658c7923bf47af8072fc9f112ae0b89850f1e
krishp058 <74632222+krishp058@users.noreply.github.com>
2021-04-23 14:42:30 +1000

    Reverted back into old format - Fixed Divide operation

--- main.py
+++ main.py
@@ -1,48 +1,48 @@
-class CasCalc:
-    '''Format list'''
-    def List(self, rawNumbers):
-        formatedNumbers = rawNumbers.split()
-        try:
-            formatedResult = [int(formatedNumbers) for formatedNumbers in formatedNumbers]
-        except:
-            print("error: Please input a number")
-        return (formatedResult)
-    
-    '''Perform operations'''
-    def operation(self, inputNumbers):
-        operatation = input("What operation would you like? (+, -, *, /): ")
-        result = 0
-        result_minus = inputNumbers[0]
-        result_times = 1
-        result_divide = inputNumbers[0]
-        try:
-            if operatation == "+":
-                for number in inputNumbers:
-                    result += number
-                print('Result: {}'.format(result))
+def start():
+    numbers = input("What are the numbers do you want to calculate (please seperated by a space): ")
+    return numbers
 
-            elif operatation == "-":
-                for number in inputNumbers[1:]:
-                    result_minus -= number
-                print('Result: {}'.format(result_minus))
 
-            elif operatation == "*":
-                for number in inputNumbers:
-                    result_times *= number
-                print('Result: {}'.format(result_times))
+def List(rawNumbers):
+    formatedNumbers = rawNumbers.split()
+    try:
+        formatedResult = [int(formatedNumbers) for formatedNumbers in formatedNumbers]
+    except:
+        print("error: Please input a number")
+    return (formatedResult)
 
-            elif operatation == "/":
-                for number in inputNumbers:
-                    result_divide /= number
-                print('Result: {}'.format(result_times))
-        except Exception:
-            print("Error: Please enter proper operations or refer back to the list.")
 
-    '''Main method.'''
-    def main(self):
-        numbers = input("What are the numbers do you want to calculate (please seperated by a space): ")
-        self.operation(self.List(numbers))
+def operation(inputNumbers):
+    operatation = input("What operation would you like? (+, -, *, /): ")
 
-if __name__ == '__main__':
-    cascalc = CasCalc()
-    cascalc.main()
+    result = 0
+    result_minus = inputNumbers[0]
+    result_times = 1
+    result_divide = inputNumbers[0]
+
+    # Operations
+    try:
+        if operatation == "+":
+            for number in inputNumbers:
+                result += number
+            print(result)
+
+        elif operatation == "-":
+            for number in inputNumbers[1:]:
+                result_minus -= number
+            print(result_minus)
+
+        elif operatation == "*":
+            for number in inputNumbers:
+                result_times *= number
+            print(result_times)
+
+        elif operatation == "/":
+            for number in inputNumbers[1:]:
+                result_divide /= number
+            print(result_divide)
+    except:
+        print("Error: Please enter proper operations or refer back to the list.")
+
+
+operation(List(start()))
